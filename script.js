b3d3fded502df36de774a7102c3a5e6c0fec8712
Tim Rundström <timmi.rundstrom@gmail.com>
2019-04-17 14:45:53 +0200

    .

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
 const unmarked = "#333";
 const marked = "#444";
 const marked_border = "#f44";
-const $ws = $("#wSection")
-const $ps = $("#pSection")
-const $gs = $("#gSection")
-const $wb = $("#wButton")
-const $pb = $("#pButton")
-const $gb = $("#gButton")
+const $ws = $("#wSection");
+const $ps = $("#pSection");
+const $gs = $("#gSection");
+const $wb = $("#wButton");
+const $pb = $("#pButton");
+const $gb = $("#gButton");
+const $Btn = $(".Btn");
 
 $(function(){
     $ws.show();
     $wb.css("border-color", marked_border);
 
     $wb.click(function(){
+        $Btn.removeClass("marked");
+        $(this).addClass("marked");
         hideSections();
         $ws.show();
         unmarkSection();
         markBtn($wb);
     });
     $pb.click(function(){
+        $Btn.removeClass("marked");
+        $(this).addClass("marked");
         hideSections();
         $ps.show();
         unmarkSection();
         markBtn($pb);
     });
     $gb.click(function(){
+        $Btn.removeClass("marked");
+        $(this).addClass("marked");
         hideSections();
         $gs.show();
         unmarkSection();
         markBtn($gb);
     });
+    $(".Btn").mouseenter(function(){
+        $(this).css("background-color", marked);
+    });
+    $(".Btn").mouseleave(function(){
+        if($(this).hasClass("marked") === false){
+            $(this).css("background-color", unmarked);
+        }
+    });
+
 });
 
 function hideSections(){
@@ -38,7 +54,6 @@ function hideSections(){
     $gs.hide();
 }
 function unmarkSection(){
-    // for loop for easier readability?
     $wb.css("background-color", unmarked);
     $wb.css("border-color", "black");
     $pb.css("background-color", unmarked);
